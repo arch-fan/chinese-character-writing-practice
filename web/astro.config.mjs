d8e1b5690ac0ebe50429a3bdeea82d5ecdd01dd4
arch-fan <juanserracines@gmail.com>
2024-04-04 20:33:10 +0200

    Removed preact

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://arch-fan.github.io",
   base: "chinese-character-writing-practice",
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind()],
 });
